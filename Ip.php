7b6e7b90b422a12069a1111fdbcafc5a6b7c8473
Manuel Flores Alatorre <manuel.flores@fahorro.com.mx>
2016-07-07 01:31:16 -0500

    Initial commit

---
+++ Ip.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuel.flores
+ * Date: 02/07/2016
+ * Time: 09:30 PM
+ */
+
+//**for($i=0;$i<=10;$i+=1)
+//*{
+   echo  'exec("runas /user:Administrador C:/xampp/htdocs/bats/ip_dynamic.bat")';
+
+//*}
+
